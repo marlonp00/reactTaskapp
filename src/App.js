f86ac1f40a012f8b4ea8e40c1c24dd3ac8eaabd9
Marlon Perini <marlon.perini@gmail.com>
2021-12-23 17:02:57 +0100

    Created new method to add Tasks

--- src/App.js
+++ src/App.js
@@ -27,29 +27,30 @@ function App() {
 ])
 // Add Task
 const addTask = (task) => {
-  console.log(task);
+  //console.log(task);
+  const id = Math.floor(Math.random() * 10000) +1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
 }
-
 
 // Delete Task
 const deleteTask = (id) => {
-
- setTasks(tasks.filter((task) => task.id !== id))
-  }
+  setTasks(tasks.filter((task) => task.id !== id))
+}
 
   // Toggle reminder
-  const toggleReminder = (id) => {
-    setTasks(
-      tasks.map((task) => 
-      task.id === id ? {...task, reminder: !task.reminder} : task))
-  }
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => 
+    task.id === id ? {...task, reminder: !task.reminder} : task))
+}
 
 
   return (
     <div className="container">
      <Header />
      <AddTask onAdd={addTask} />
-    { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to show'} 
+    { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}  /> : 'No Tasks to show'} 
     
     </div>
   );
